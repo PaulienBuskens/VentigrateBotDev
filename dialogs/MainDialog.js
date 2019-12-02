3bcb57d0959573b521dc0150e156bfcae754345d
Paulien Buskens <Paulien.buskens1@hotmail.com>
2019-12-02 09:58:09 +0100

    graphaxios

--- dialogs/MainDialog.js
+++ dialogs/MainDialog.js
@@ -8,6 +8,8 @@ const OAUTH_PROMPT = 'oAuthPrompt';
 const CHOICE_PROMPT = 'choicePrompt';
 const TEXT_PROMPT = 'textPrompt';
 
+const express = require('express');
+
 class MainDialog extends LogoutDialog {
     constructor() {
         super('MainDialog');
@@ -68,16 +70,34 @@ class MainDialog extends LogoutDialog {
     }
 
     async graph(context,next){
-        const options = {
-	        authProvider,
+        const APP_ID = 'e414d507-6c1d-4b7c-baa4-b0b8834e6d9c';
+        const APP_SECERET = '+_#7mr=h:MTNo!a2YaR%0Pi8bD89PxT';
+        const TOKEN_ENDPOINT= 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
+        const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
+
+        const axios = require('axios');
+        const qs = require('qs');
+
+        const postData = {
+            client_id: APP_ID,
+            scope: MS_GRAPH_SCOPE,
+            client_secret: APP_SECERET,
+            grant_type: 'client_credentials'
         };
 
-        const client = Client.init(options);
+        axios.defaults.headers.post['Content-Type'] =
+            'application/x-www-form-urlencoded';
 
-        let res = await client.api('/me/')
-	    .get();
+        let token = '';
 
-        await context.sendActivity(res);
+        axios
+        .post(TOKEN_ENDPOINT, qs.stringify(postData))
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 
     async promptStep(step) {
