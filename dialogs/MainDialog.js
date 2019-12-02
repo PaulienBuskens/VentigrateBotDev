91c3f73aed3c3a4f89272b972ab4b302f7eaf8f9
Paulien Buskens <Paulien.buskens1@hotmail.com>
2019-12-02 10:15:54 +0100

    graphclient

--- dialogs/MainDialog.js
+++ dialogs/MainDialog.js
@@ -1,6 +1,7 @@
 const { ChoicePrompt, DialogSet, DialogTurnStatus, OAuthPrompt, TextPrompt, WaterfallDialog } = require('botbuilder-dialogs');
 const { LogoutDialog } = require('./logoutDialog');
 const { OAuthHelpers } = require('../oAuthHelpers');
+const { MyAuthenticationProvider } = require('../MyAuthenticationProvider');
 const { MessageFactory, CardFactory,TurnContext,ActivityHandler } = require('botbuilder');
 
 const MAIN_WATERFALL_DIALOG = 'mainWaterfallDialog';
@@ -70,35 +71,14 @@ class MainDialog extends LogoutDialog {
     }
 
     async graph(context,next){
-        const APP_ID = 'e414d507-6c1d-4b7c-baa4-b0b8834e6d9c';
-        const APP_SECERET = '+_#7mr=h:MTNo!a2YaR%0Pi8bD89PxT';
-        const TOKEN_ENDPOINT= 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
-        const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
-
-        const axios = require('axios');
-        const qs = require('qs');
-
-        const postData = {
-            client_id: APP_ID,
-            scope: MS_GRAPH_SCOPE,
-            client_secret: APP_SECERET,
-            grant_type: 'client_credentials'
+        let clientOptions: ClientOptions = {
+	        authProvider: new MyCustomAuthenticationProvider(),
         };
+        const client = Client.initWithMiddleware(clientOptions);
 
-        axios.defaults.headers.post['Content-Type'] =
-            'application/x-www-form-urlencoded';
-
-        let token = '';
-
-        axios
-        .post(TOKEN_ENDPOINT, qs.stringify(postData))
-        .then(response => {
-            console.log(response.data);
-            await context.sendActivity(response.data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+        
+        let res = await client.api('/me/')
+	    .get();
     }
 
     async promptStep(step) {
