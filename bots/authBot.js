cefa50582992be4744ef19135ad2e1d01fada03e
Paulien Buskens <Paulien.buskens1@hotmail.com>
2019-12-17 12:54:53 +0100

    teamscontext

--- bots/authBot.js
+++ bots/authBot.js
@@ -7,10 +7,16 @@ class AuthBot extends DialogBot {
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
+            
             for (let cnt = 0; cnt < membersAdded.length; cnt++) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity('Welcome to the VentigrateBot. Type anything to get started.');
-                    
+                     const teamDetails = await TeamsInfo.getTeamDetails(context);
+                    if (teamDetails) {
+                        await turnContext.sendActivity(`The group ID is: ${teamDetails.aadGroupId}`);
+                    } else {
+                        await turnContext.sendActivity('This message did not come from a channel in a team.');
+                    }
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
