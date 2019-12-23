e302b0e193efd3803b7d4dc1891561c2b2ff871b
Paulien Buskens <Paulien.buskens1@hotmail.com>
2019-12-23 15:01:54 +0100

    clean up

--- bots/dialogBot.js
+++ bots/dialogBot.js
@@ -58,7 +58,7 @@ class DialogBot extends ActivityHandler {
 
             } else if(context.activity.text.includes("@bored")){    
                 await this.dialog.bored(context,this.dialogState);
-
+                
             }else{
                 await this.dialog.run(context, this.dialogState);
             }
